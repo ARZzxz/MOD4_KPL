a119a6df0d0e376e98b7f186ed92efcbbaf9e8fb
ARZzxz <achmadrizky0211@gmail.com>
2022-03-19 14:25:06 +0700

    menambahkan class SimpleDataBase

--- Jurnal_MOD4_1302200027/Program.cs
+++ Jurnal_MOD4_1302200027/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Jurnal_MOD4_1302200027
+{
+    public class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T databaru)
+        {
+            storedData.Add(databaru);
+            inputDates.Add(DateTime.Now);
+        }
+    }
