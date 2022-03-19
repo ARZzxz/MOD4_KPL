64ebfbf1918be4ea90a2dba71626a336a866a9bb
ARZzxz <achmadrizky0211@gmail.com>
2022-03-19 14:10:38 +0700

    menambahkan class Penjumlahan dengan methodJumlahTigaAngka”.

--- Jurnal_MOD4_1302200027/Program.cs
+++ Jurnal_MOD4_1302200027/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jurnal_MOD4_1302200027
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            SimpleDataBase<int> data = new SimpleDataBase<double>();
+
+        }
+    }
+}
+
+public class penjumlahan
+{
+    public static void jmlTigaAngka<T>(T data1, T data2, T data3)
+    {
+        dynamic angkaP = data1;
+        dynamic angkaD = data2;
+        dynamic angkaT = data3;
+
+        Console.WriteLine(angkaP + angkaD + angkaT + "\n");
+
+    }
+}
